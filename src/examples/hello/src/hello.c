7469e198222670989e1bf4a23e8869ab3ef498be
bkbme <bernhard@kiesbauer.com>
2012-07-13 00:12:40 +0200

    Changed upload.py to make CPU reset before uploading opcode. Fixed hello example.

--- src/examples/hello/src/hello.c
+++ src/examples/hello/src/hello.c
@@ -5,11 +5,9 @@ void main()
   //Full speed ahead (48MHz)
   CPUCS = (1<<CLKSPD1);
 
+  DDRA = 0xff;
   while(1) {
-    DDRA = 0xff;
-    while(1) {
-      PORTA_1 = 1;
-      PORTA_1 = 0;
-    }
+    PORTA_0 = 1;
+    PORTA_0 = 0;
   }
 }
--- src/tools/upload.py
+++ src/tools/upload.py
@@ -3,6 +3,7 @@ import usb.core
 import usb.util
 from intelhex import IntelHex
 import sys
+import time
  
 dev = usb.core.find(idVendor=0x04b4, idProduct=0x8613)
  
@@ -14,12 +15,13 @@ def upload(filename):
 def setReset(reset):
   #Get device out of reset
   if reset:
-    dev.ctrl_transfer(0x40, 0xa0, 0xe600, 0xff , [0])
+    dev.ctrl_transfer(0x40, 0xa0, 0xe600, 0 , [0xff])
   else:
     dev.ctrl_transfer(0x40, 0xa0, 0xe600, 0 , [0])
 
 if(len(sys.argv) > 2):
   if(sys.argv[1] == "upload"):
+    setReset(True)
     upload(sys.argv[2])
     setReset(False)
   elif(sys.argv[1] == "reset"):
